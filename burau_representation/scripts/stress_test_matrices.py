f03eaf2da1f0843b484d7471040ee4a12f5611fc
Borys Holikov <borisgolikov@Boriss-Laptop.local>
2025-04-09 17:34:00 +0300

    a

--- burau_representation/scripts/stress_test_matrices.py
+++ burau_representation/scripts/stress_test_matrices.py
@@ -9,12 +9,24 @@ from multiprocessing import freeze_support
 if __name__ == '__main__':
     freeze_support()
     mod = 5
-    n = 11
+    n = 9
     symbol_to_matrix = {
         "A": lmp.A,
         "B": lmp.B,
         "a": lmp.a,
         "b": lmp.b
     }
-    a = bf.extend_in_all_ways_p(symbol_to_matrix,symbol_to_matrix,9)
-    print(len(a))
+    start_time = time.perf_counter()
+    a = bf.calculate_products(symbol_to_matrix,n)
+    end_time = time.perf_counter()
+    print(start_time-end_time)
+
+    start_time = time.perf_counter()
+    a = bf.extend_in_all_ways_p(symbol_to_matrix,symbol_to_matrix,n)
+    end_time = time.perf_counter()
+    print(start_time-end_time)
+
+    start_time = time.perf_counter()
+    a = bf.extend_in_all_ways(symbol_to_matrix,symbol_to_matrix,n)
+    end_time = time.perf_counter()
+    print(start_time-end_time)
