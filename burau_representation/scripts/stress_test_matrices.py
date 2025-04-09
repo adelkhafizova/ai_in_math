32274782b2aab2879c38ab06ccd6266f720c31f9
Borys Holikov <borisgolikov@Boriss-Laptop.local>
2025-04-09 03:33:32 +0300

    a

--- burau_representation/scripts/stress_test_matrices.py
+++ burau_representation/scripts/stress_test_matrices.py
@@ -16,8 +16,5 @@ if __name__ == '__main__':
         "a": lmp.a,
         "b": lmp.b
     }
-    a = bf.calculate_products(symbol_to_matrix,n-1)
+    a = bf.extend_in_all_ways_p(symbol_to_matrix,symbol_to_matrix,9)
     print(len(a))
-    time.sleep(10)
-    b = bf.extend_in_all_ways_p(symbol_to_matrix,a,1)
-
