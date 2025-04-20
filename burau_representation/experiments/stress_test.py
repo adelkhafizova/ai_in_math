a7b368e81b340e39ccdc885846fcedf785880f15
Borys Holikov <borisgolikov@Boriss-Laptop.local>
2025-04-20 10:52:52 +0200

    slightly improved organization

---
+++ burau_representation/experiments/stress_test.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('../scripts')
+import multiprocessing
+import time
+import burau_enchanced as b
+
+# Function to run in each process (will just run an infinite loop)
+def cpu_stress():
+    while True:
+        b.A**10000
+        pass  # The busy-wait loop that will stress the CPU
+
+if __name__ == '__main__':
+    # Get the number of physical cores in the system
+    num_processes = multiprocessing.cpu_count()  # Number of cores (logical CPUs)
+
+    # Create and start one process per core
+    processes = []
+    for _ in range(num_processes):
+        p = multiprocessing.Process(target=cpu_stress)
+        p.start()
+        processes.append(p)
+
+    # Optional: Keep the main process alive while child processes are running
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # When you press Ctrl+C, terminate all the processes
+        for p in processes:
+            p.terminate()
+        print("Test finished.")
